6b5576ce28bf9f7efef4bd034d05b4d1130fff99
Kathy Bailey Hines <135627670+klbailey@users.noreply.github.com>
2023-12-03 16:03:46 +0000

    Add icons.js updated index.js

---
+++ src/js/component/icons.js
@@ -0,0 +1,57 @@
+/* global FontAwesomeConfig */
+
+/**
+ * 1) First you have to import the @fortawesome/fontawesome library
+*/
+import fontawesome from '@fortawesome/fontawesome';
+
+
+/**
+ * 2) Then you have to import every icon that you will use
+*/
+import faClock from '@fortawesome/fontawesome-free-regular/faClock';
+// import faCheck from '@fortawesome/fontawesome-free-solid/faCheck';
+// import faTimes from '@fortawesome/fontawesome-free-solid/faTimes';
+// import faSync from '@fortawesome/fontawesome-free-solid/faSync';
+// import faEllipsisV from '@fortawesome/fontawesome-free-solid/faEllipsisV';
+// import faGraduationCap from '@fortawesome/fontawesome-free-solid/faGraduationCap';
+// import faPlay from '@fortawesome/fontawesome-free-solid/faPlay';
+// import faCog from '@fortawesome/fontawesome-free-solid/faCog';
+// import faSpinner from '@fortawesome/fontawesome-free-solid/faSpinner';
+// import faSearch from '@fortawesome/fontawesome-free-solid/faSearch';
+// import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+// import faExternalLinkAlt from '@fortawesome/fontawesome-free-solid/faExternalLinkAlt';
+// import faArrowRight from '@fortawesome/fontawesome-free-solid/faArrowRight';
+// import faArrowLeft from '@fortawesome/fontawesome-free-solid/faArrowLeft';
+// import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
+// import faAngleDoubleDown from '@fortawesome/fontawesome-free-solid/faAngleDoubleDown';
+// import faAngleDoubleUp from '@fortawesome/fontawesome-free-solid/faAngleDoubleUp';
+// import faQuestion from '@fortawesome/fontawesome-free-solid/faQuestion';
+// import faExclamationCircle from '@fortawesome/fontawesome-free-solid/faExclamationCircle';
+// import faYoutube from '@fortawesome/fontawesome-free-brands/faYoutube';
+// import faCode from '@fortawesome/fontawesome-free-solid/faCode';
+// import faSlack from '@fortawesome/fontawesome-free-brands/faSlack';
+// import faCalendarAlt from '@fortawesome/fontawesome-free-solid/faCalendarAlt';
+
+/**
+ * 3) Tell font-awesome that you want to replace your icons with SVGs (recomended for performance)
+*/
+fontawesome.config = {
+  autoReplaceSvg: 'nest'
+};
+
+/**
+ * 4) Add the icons into the font-awesome library
+*/
+fontawesome.library.add(faClock
+    // faCheck, faGraduationCap, faPlay, faSpinner, faSearch, faGithub, faEnvelope,
+    // faCog, faSync, faEllipsisV, faExternalLinkAlt, faArrowRight, faArrowLeft,
+    // faAngleDoubleDown, faAngleDoubleUp, faQuestion, faExclamationCircle,
+    // faYoutube, faCode, faTimes, faSlack, faCalendarAlt
+);
+
+
+/**
+ * 5) Now, you can place the icon tag anywhere you want to icon to show, for example:
+ *    <i className="fas fa-check"></i>
+*/
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,51 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+// import Home from "./component/home.jsx";
+// import "./component/icons.js";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+function SimpleCounter(props) {
+    // do html
+    return (
+        <div className="bigCounter">
+            <div className="calendar">
+                <i className="far fa-clock" />
+                {/* // <FontAwesomeIcon icon={regular("clock")} /> */}
+            </div>
+            <div className="four">{props.digitFour}</div>
+            <div className="three">{props.digitThree}</div>
+            <div className="two">{props.digitTwo}</div>
+            <div className="one">{props.digitOne}</div>
+        </div>
+        );
+}
+
+SimpleCounter.propTypes = {
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number
+};
+
+let counter = 0;
+// re-render the component every second
+setInterval(function() {
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+    //render your react application
+    ReactDOM.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, 
+        document.querySelector("#app")
+    );
+},1000);
+//set every 1k seconds
+
